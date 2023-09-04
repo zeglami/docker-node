bff6239e6ba0b4b230794c61af677aa39de93a3f
zeglami <zeglami.abdelhamid@gmail.com>
2023-09-04 09:45:04 +0200

    volumes

--- api/app.js
+++ api/app.js
@@ -13,7 +13,7 @@ app.get('/', (req, res) => {
     },
     {
       "id":"2",
-      "title":"Game Review: Pokemon Brillian Diamond"
+      "title":"Game Review: Pokemon Brilliantt Diamond."
     },
     {
       "id":"3",
